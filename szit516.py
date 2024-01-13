6eef2ba65deb7776c085d3c079b6db7a7935d71f
patrikkalman <120101231+patrikkalman@users.noreply.github.com>
2024-01-13 21:52:35 +0100

    Add files via upload

---
+++ szit516.py
@@ -0,0 +1,9 @@
+f=open("szit516.txt")
+for sor in f:
+    print(sor)
+
+    #vagy
+print()
+lista=["@ @ @ @ @ @ @ @ @","  @ @ @ @ @ @ @ ","    @ @ @ @ @","      @ @ @","        @"]
+for i in lista:
+    print(i)
