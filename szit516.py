b3200d314aafb5c06ba299a0706db93dd9d29155
patrikkalman <120101231+patrikkalman@users.noreply.github.com>
2024-01-13 21:53:16 +0100

    Update szit516.py

--- szit516.py
+++ szit516.py
@@ -2,8 +2,4 @@ f=open("szit516.txt")
 for sor in f:
     print(sor)
 
-    #vagy
-print()
-lista=["@ @ @ @ @ @ @ @ @","  @ @ @ @ @ @ @ ","    @ @ @ @ @","      @ @ @","        @"]
-for i in lista:
-    print(i)
+  
