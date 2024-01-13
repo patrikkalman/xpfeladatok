8b1fcb0e6c000edd9eccbf62e3b5df1b049495ee
patrikkalman <120101231+patrikkalman@users.noreply.github.com>
2024-01-13 15:56:49 +0100

    Add files via upload

--- szit50.py
+++ szit50.py
@@ -1,3 +1,21 @@
 lista=["----------------------","|3 db  végzáró  450 € |","|5 db  pálca    250 € |","|4 db  k zsinór 600 € |","|1 cm³ z lé     700 € |","|2 m²  vitorla  490 € |","----------------------"]
 for i in lista:
     print(i)
+
+    #vagy a masik megoldas
+
+lista1=(" ----------------------")
+lista2=("| 3 db  végzáró  450 € |")
+lista3=("| 5 db  pálca    250 € |")
+lista4=("| 4 db  k zsinór 600 € |")
+lista5=("| 1 cm³ z lé     700 € |")
+lista6=("| 2 m²  vitorla  490 € |")
+lista7=(" ----------------------")
+
+print(lista1)
+print(lista2)
+print(lista3)
+print(lista4)
+print(lista5)
+print(lista6)
+print(lista7)
