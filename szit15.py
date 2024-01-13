41ba62ec0539a6e7cf5dcc7cbc5e103d2edc219c
patrikkalman <120101231+patrikkalman@users.noreply.github.com>
2024-01-13 15:22:35 +0100

    Add files via upload

---
+++ szit15.py
@@ -0,0 +1,6 @@
+print("Kálmán Patrik \n 2024.01.13")
+lista=["        __ ","      _ || ","     / \|| ","    /   \| ","   /     \ ","   |     | ","   |     | ","   ------- "
+]
+for sor in lista:
+    print(sor)
+
