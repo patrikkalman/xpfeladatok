a58f23b5e98d6e3fa3f73fa687aa8dc1fe76f5c5
patrikkalman <120101231+patrikkalman@users.noreply.github.com>
2024-01-14 11:56:35 +0100

    Update szit23.py

--- szit23.py
+++ szit23.py
@@ -1,4 +1,24 @@
 print("Szerzo:Kálmán Patrik")
-f=open("szit13.txt")
-for sor in f:
-    print(sor)
+print()
+lista1=[ "           ##############"]
+lista2=["          #            # #             #######"]
+lista3=["         #            # #              #######"]
+lista4=["        #            # #                #   #"]
+lista5=["       #            # #                #     #"]
+lista6=["      #            # #                #       #"]
+lista7=["     #            # #                #         #"]
+lista8=["    ############## #                 #         #"]
+lista9=["   #            # #                  #         #"]
+lista10=["    ##############                   ###########"]
+
+
+print(lista1)
+print(lista2)
+print(lista3)
+print(lista4)
+print(lista5)
+print(lista6)
+print(lista7)
+print(lista8)
+print(lista9)
+print(lista10)
