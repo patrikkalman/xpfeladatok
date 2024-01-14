16dff6c8d99aac9dbc0870252a4627d116e02d4b
patrikkalman <120101231+patrikkalman@users.noreply.github.com>
2024-01-14 19:00:15 +0100

    Add files via upload

---
+++ szit31.py
@@ -0,0 +1,14 @@
+def rajzol():
+    lines = [
+        " ______ ",
+        "()_____)",
+        " \\     \\",
+        "  \\     \\",
+        "   \\_____\\",
+        "   ()_____)"
+    ]
+
+    for line in lines:
+        print(line)
+
+rajzol()
