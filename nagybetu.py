6299802dd1db5c2bcdb0976d7068e7288898dc0f
patrikkalman <120101231+patrikkalman@users.noreply.github.com>
2024-01-15 18:07:35 +0100

    Update nagybetu.py

--- nagybetu.py
+++ nagybetu.py
@@ -1,3 +1,8 @@
 nagybetu = input("Kérem, adjon meg egy nagybetűt: ")
-kisbetu = nagybetu.lower()
-print(f"A kisbetűs változat: {kisbetu}")
+
+if 'A' <= nagybetu <= 'Z':
+    kisbetusALAK = ord(nagybetu) + ord('a') - ord('A')
+    kisbetu = chr(kisbetusALAK)
+    print(f"A kisbetűs változat: {kisbetu}")
+else:
+    print("Hibás bemenet! Kérlek, adj meg egy nagybetűt.")
