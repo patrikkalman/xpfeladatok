dd5d433b52847263cdab9b1ab7a0a81bbe95131b
patrikkalman <120101231+patrikkalman@users.noreply.github.com>
2024-01-15 17:50:10 +0100

    Update szit656.py

--- szit656.py
+++ szit656.py
@@ -3,7 +3,7 @@ karakterlanc = input("Kérem, adjon meg egy karakterláncot: ")
 maganhangzokszama = 0
 
 for karakter in karakterlanc:
-    if karakter.lower() in 'aeiou':
+    if karakter.lower() in 'aáeéiíoóöőuúüű':
         maganhangzokszama += 1
 
 print(f"A karakterláncban {maganhangzokszama} magánhangzó található.")
